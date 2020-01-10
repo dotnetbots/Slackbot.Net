983279948c1ce4f3d358e0d94272ea1f8c4a9302
Korsneso <johnkors@gmail.com>
2020-01-10 11:53:02 +0100

    Adds more props to OAuthAccess response

--- source/src/Slackbot.Net.SlackClients.Http/Models/Responses/OAuthAccess/OAuthAccessResponse.cs
+++ source/src/Slackbot.Net.SlackClients.Http/Models/Responses/OAuthAccess/OAuthAccessResponse.cs
@@ -4,5 +4,14 @@ namespace Slackbot.Net.SlackClients.Http.Models.Responses.OAuthAccess
     {
         public string AccessToken { get; set; }
         public string Scope { get; set; }
+        public string Team_Name { get; set; }
+        public string Team_Id { get; set; }
+        public Bot Bot { get; set; }
+    }
+
+    public class Bot
+    {
+        public string Bot_User_Id { get; set; }
+        public string Bot_Access_Token { get; set; }
     }
 }
