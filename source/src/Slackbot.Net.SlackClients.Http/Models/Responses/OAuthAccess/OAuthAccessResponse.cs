33b0e3529a0f49ad83254e70f41252e3b3e67902
John Korsnes <johnkors@gmail.com>
2020-01-10 11:55:53 +0100

    Add oauth access (#1)
    * Adds oauth.access
    
    * Adds more props to OAuthAccess response

---
+++ source/src/Slackbot.Net.SlackClients.Http/Models/Responses/OAuthAccess/OAuthAccessResponse.cs
@@ -0,0 +1,17 @@
+namespace Slackbot.Net.SlackClients.Http.Models.Responses.OAuthAccess
+{
+    public class OAuthAccessResponse : Response
+    {
+        public string AccessToken { get; set; }
+        public string Scope { get; set; }
+        public string Team_Name { get; set; }
+        public string Team_Id { get; set; }
+        public Bot Bot { get; set; }
+    }
+
+    public class Bot
+    {
+        public string Bot_User_Id { get; set; }
+        public string Bot_Access_Token { get; set; }
+    }
+}
