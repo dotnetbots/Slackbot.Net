7f35460eb619cfb59df7a473987f2a4aa3c59070
John Korsnes <johnkors@gmail.com>
2021-09-29 13:12:27 +0200

    Re-adds no-token-store function

--- Samples/HelloWorld/Program.cs
+++ Samples/HelloWorld/Program.cs
@@ -1,9 +1,8 @@
-using System.Threading.Tasks;
 using Slackbot.Net.Endpoints.Hosting;
 using Slackbot.Net.Endpoints.Authentication;
 using Slackbot.Net.Endpoints.Abstractions;
 using Slackbot.Net.Endpoints.Models.Events;
-using Slackbot.Net.Abstractions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +11,7 @@ builder.Services.AddAuthentication()
                 .AddSlackbotEvents(c => c.SigningSecret = Environment.GetEnvironmentVariable("CLIENT_SIGNING_SECRET"));
 
 // Setup event handlers
-builder.Services.AddSlackBotEvents<TokenStore>()
+builder.Services.AddSlackBotEvents()
                 .AddAppMentionHandler<DoStuff>();
 
 
@@ -26,18 +25,7 @@ class DoStuff : IHandleAppMentions
 
     public Task<EventHandledResponse> Handle(EventMetaData eventMetadata, AppMentionEvent slackEvent)
     {
-        // handler code executed given ShouldHandle returns true
         Console.WriteLine("Doing stuff!");
         return Task.FromResult(new EventHandledResponse("yolo"));
     }
-}
-
-class TokenStore : ITokenStore
-{
-    public string SlackToken = Environment.GetEnvironmentVariable("SLACK_TOKEN");
-
-    public Task<IEnumerable<string>> GetTokens() => Task.FromResult(new [] { SlackToken }.AsEnumerable());
-    public Task<string> GetTokenByTeamId(string teamId) => Task.FromResult(SlackToken);
-    public Task Delete(string token) => throw new NotImplementedException("Single workspace app");
-    public Task Insert(Workspace slackTeam) => throw new NotImplementedException("Single workspace app");
 }
