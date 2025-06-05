19e0ecc106a2321334a87c5b9c9dcb076e003744
Ole Magnus Urdahl <murdahl89@gmail.com>
2025-06-05 23:19:30 +0200

    Add interactive block action attributes
    https://api.slack.com/reference/interaction-payloads/block-actions#payload-fields

--- source/src/Slackbot.Net.Endpoints/Models/Interactive/BlockActions/BlockAction.cs
+++ source/src/Slackbot.Net.Endpoints/Models/Interactive/BlockActions/BlockAction.cs
@@ -1,3 +1,4 @@
+using Slackbot.Net.Endpoints.Models.Interactive.MessageActions;
 using Slackbot.Net.Models.BlockKit;
 
 namespace Slackbot.Net.Endpoints.Models.Interactive.BlockActions;
@@ -6,6 +7,13 @@ public class BlockActionInteraction : Interaction
 {
     public Team Team { get; set; }
     public User User { get; set; }
-
+    public Channel Channel { get; set; }
+    public Message Message { get; set; }
+    public State State { get; set; }
     public IEnumerable<ActionsBlock> Actions { get; set; }
+}
+
+public class State
+{
+    public Dictionary<string, string> Values { get; set; } = [];
 }
