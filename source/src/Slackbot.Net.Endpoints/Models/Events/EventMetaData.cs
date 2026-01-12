fc06b8c3e12162cff7fef3e99f86cf1bd0683a5e
Ole Magnus Urdahl <murdahl89@gmail.com>
2026-01-12 15:53:40 +0100

    Update EventMetaData.cs

--- source/src/Slackbot.Net.Endpoints/Models/Events/EventMetaData.cs
+++ source/src/Slackbot.Net.Endpoints/Models/Events/EventMetaData.cs
@@ -5,11 +5,11 @@ public class EventMetaData
     public string Token { get; set; }
     public string Team_Id { get; set; }
     public string Type { get; set; }
-    public string[] AuthedUsers { get; set; }
+    public string[] AuthedUsers { get; set; } = [];
     public string Event_Id { get; set; }
     public long Event_Time { get; set; }
     public long Api_App_Id { get; set; }
-    public Authorization[] Authorizations { get; set; }
+    public Authorization[] Authorizations { get; set; } = [];
     public bool Is_Ext_Shared_Channel { get; set; }
 }
 
