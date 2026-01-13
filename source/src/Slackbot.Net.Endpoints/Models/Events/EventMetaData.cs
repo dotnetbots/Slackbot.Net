b73d8e5273faf39e34456ddbc0f1a7ce1d09824c
Ole Magnus Urdahl <murdahl89@gmail.com>
2026-01-13 08:09:32 +0100

    Add new fields to EventMetaData and Authorization class (#32)

--- source/src/Slackbot.Net.Endpoints/Models/Events/EventMetaData.cs
+++ source/src/Slackbot.Net.Endpoints/Models/Events/EventMetaData.cs
@@ -5,7 +5,19 @@ public class EventMetaData
     public string Token { get; set; }
     public string Team_Id { get; set; }
     public string Type { get; set; }
-    public string[] AuthedUsers { get; set; }
+    public string[] AuthedUsers { get; set; } = [];
     public string Event_Id { get; set; }
     public long Event_Time { get; set; }
+    public long Api_App_Id { get; set; }
+    public Authorization[] Authorizations { get; set; } = [];
+    public bool Is_Ext_Shared_Channel { get; set; }
+}
+
+public class Authorization
+{
+    public string Enterprise_Id { get; set; }
+    public string Team_Id { get; set; }
+    public string User_Id { get; set; }
+    public bool Is_Bot { get; set; }
+    public bool Is_Enterprise_Install { get; set; }
 }
