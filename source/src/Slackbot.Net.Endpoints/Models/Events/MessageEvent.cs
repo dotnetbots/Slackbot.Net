72428cad6e2180db374f9fab6a62fbc52f7cdd07
John Korsnes <johnkors@gmail.com>
2026-01-16 15:57:36 +0100

    message(bots): add bot_id to message.im payload
    being able to diff if bot or not

--- source/src/Slackbot.Net.Endpoints/Models/Events/MessageEvent.cs
+++ source/src/Slackbot.Net.Endpoints/Models/Events/MessageEvent.cs
@@ -8,4 +8,13 @@ public class MessageEvent : SlackEvent
     public string Ts { get; set; }
     public string Event_Ts { get; set; }
     public string Channel_Type { get; set; }
+
+    public string? Bot_id { get; set; }
+    public BotProfile Bot_Profile { get; set; }
+}
+
+public class BotProfile
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
 }
